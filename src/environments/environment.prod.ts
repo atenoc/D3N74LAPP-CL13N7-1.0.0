d4ed3f2890a19a3698bdb3ac02375b39a74963d9
Car <car.ateno@gmail.com>
2023-03-10 12:36:07 -0600

    Ajustes alta de usuario

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
-const ipHost:string ="http://localhost:4000"
+//const ipHost:string ="http://localhost:4000"
+const ipHost:string ="https://d3n74l4pp-4p1-production.up.railway.app"		
 
 export const environment = {
   production: true,
