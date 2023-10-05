013e1f1f39da0bf4a60c7260ba88b37ff904ecfb
“atenoc” <car.ateno@gmail.com>
2023-10-05 14:13:28 -0600

    Ajuste sidebar acordeon / header

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -4,6 +4,7 @@ import { Usuario } from 'src/app/models/Usuario.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { SharedService } from 'src/app/services/shared.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-sidebar',
@@ -25,6 +26,7 @@ export class SidebarComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("SIDEBAR Component")
+    this.inicializarAccordion()
 
     this.usuarioService.getUsuarioByCorreo$(localStorage.getItem('correo_us')).subscribe(
       res => {
@@ -59,5 +61,37 @@ export class SidebarComponent implements OnInit {
     this.rolUsuario=""
     this.ngOnInit()
     console.log("reload navigate <-")
+  }
+
+  inicializarAccordion(): void {
+    $(function() {
+      var Accordion = function(el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
+    
+        // Variables privadas
+        var links = this.el.find('.link');
+        // Evento
+        links.on('click', { el: this.el, multiple: this.multiple }, this.dropdown);
+      };
+    
+      Accordion.prototype.dropdown = function(e) {
+        var $el = e.data.el;
+        var $this = $(this),
+          $next = $this.next();
+    
+        // Personaliza la velocidad de la animación aquí (ejemplo: 200 milisegundos)
+        var animationSpeed = 200;
+    
+        $next.slideToggle(animationSpeed);
+        $this.parent().toggleClass('open');
+    
+        if (!e.data.multiple) {
+          $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+        }
+      };
+    
+      var accordion = new Accordion($('#accordion'), false);
+    });
   }
 }
