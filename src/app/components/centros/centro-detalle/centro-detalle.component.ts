e4c10112c4148f7eee15ceb9eeb76e35e7b977b8
“atenoc” <car.ateno@gmail.com>
2023-10-17 19:40:33 -0600

    Validación campos centro-detalle

--- src/app/components/centros/centro-detalle/centro-detalle.component.ts
+++ src/app/components/centros/centro-detalle/centro-detalle.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 //import { ActivatedRoute } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Centro } from 'src/app/models/Centro.model';
 import { CentroService } from 'src/app/services/centro.service';
+import { Mensajes } from 'src/app/shared/mensajes.config';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -15,14 +17,39 @@ export class CentroDetalleComponent implements OnInit {
   id: string
   centro: Centro
 
+  formularioCentro:FormGroup
+
+  //mensajes
+  campoRequerido: string;
+  correoValido: string;
+  contrasenaLongitud: string;
+  telefonoLongitud: string;
+  soloNumeros: string;
+
   constructor(
+    private formBuilder:FormBuilder, 
     //private activatedRoute: ActivatedRoute,
     private centroService:CentroService, 
     private modalService: NgbModal
-    ) { }
+    ) {
+      this.campoRequerido = Mensajes.CAMPO_REQUERIDO;
+      this.correoValido = Mensajes.CORREO_VALIDO;
+      this.telefonoLongitud = Mensajes.TELEFONO_LONGITUD;
+      this.soloNumeros = Mensajes.SOLO_NUMEROS;
+    }
 
   ngOnInit() {
     console.log("CENTRO DETALLE Comp")
+
+    this.formularioCentro = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]+$'), Validators.minLength(10)]],
+      correo: ['', Validators.compose([
+        //Validators.required, 
+        Validators.email
+      ])],
+      direccion: ['', [Validators.required, Validators.minLength(10)]]
+    })
 
     /*this.activatedRoute.params.subscribe(params => {
       this.id = params['id'];
@@ -42,13 +69,34 @@ export class CentroDetalleComponent implements OnInit {
       res => {
         this.centro = res;
         console.log("id Centro obtenido:" + res.id)
+
+        this.formularioCentro.patchValue({
+          correo: this.centro.correo,
+          nombre: this.centro.nombre,
+          telefono: this.centro.telefono,
+          direccion: this.centro.direccion
+        });
+
       },
       err => console.log("error: " + err)
     )
   }
 
-  updateCentro(nombre: HTMLInputElement, telefono: HTMLInputElement, correo: HTMLInputElement, direccion: HTMLInputElement): boolean {
-    this.centroService.updateCentro(this.centro.id, nombre.value, telefono.value, correo.value, direccion.value)
+  getInputClass(controlName: string) {
+    const control = this.formularioCentro.get(controlName);
+    return {
+      'invalid-input': control?.invalid && control?.dirty
+    };
+  }
+
+  updateCentro(): boolean {
+    this.centroService.updateCentro(
+      this.centro.id, 
+      this.formularioCentro.value.nombre,
+      this.formularioCentro.value.telefono,
+      this.formularioCentro.value.correo,
+      this.formularioCentro.value.direccion
+      )
       .subscribe(res => {
         console.log("Centro actualizado: "+res);
 
@@ -59,9 +107,9 @@ export class CentroDetalleComponent implements OnInit {
           html:
             `<strong>${ this.centro.nombre }</strong><br/>` +
             '¡Información actualizada!',
-          showConfirmButton: true,
-          confirmButtonColor: '#28a745',
-          timer: 4000
+          showConfirmButton: false,
+          //confirmButtonColor: '#28a745',
+          timer: 2000
         })
 
       },
