ebf1b94824cd4c454842ab556a90f186cf347d0e
Car <car.ateno@gmail.com>
2024-06-13 19:35:38 -0600

    Guardar por cita / paciente

--- src/app/components/calendario/cita-edit/cita-edit.component.ts
+++ src/app/components/calendario/cita-edit/cita-edit.component.ts
@@ -27,37 +27,42 @@ export class CitaEditComponent implements OnInit {
   fecha_creacion:string
 
   fechaModelInicio: NgbDateStruct;
-  fechaModelFin: NgbDateStruct;
+  fechaModelFin?: NgbDateStruct;
   selectedTimeInicio: { hour: number, minute: number } = { hour: 0, minute: 0 };
-  selectedTimeFin: { hour: number, minute: number } = { hour: 0, minute: 0 };
+  selectedTimeFin?: { hour: number, minute: number } = { hour: 0, minute: 0 };
+
+  fechaModelInicioObtenido: NgbDateStruct;
+  fechaModelFinObtenido?: NgbDateStruct;
+  selectedTimeInicioObtenido: { hour: number, minute: number } = { hour: 0, minute: 0 };
+  selectedTimeFinObtenido?: { hour: number, minute: number } = { hour: 0, minute: 0 };
 
   nombrePaciente:string=""
   apellidoPaternoPaciente:string=""
   apellidoMaternoPaciente:string=""
   edadPaciente:string=""
   telefonoPaciente:string=""
-  titulo:string=""
+  telefonoPacienteObtenido:string=""
+  //titulo:string=""
   motivo:string=""
+  motivoObtenido:string=""
   fecha_hora_inicio:string
   fecha_hora_fin:string
   nota:string=""
-
   id_paciente:string
   //id_medico:string
 
-  mostrarMensajeNombre:boolean = true
-  mostrarMensajeApPaterno:boolean = true
-  mostrarMensajeTitulo:boolean = true
   mostrarMensajeMotivo:boolean = true
   mostrarMensajeFechaInicio:boolean = true
   mostrarMensajeHoraInicio:boolean = true
   mostrarMensajeTelefono:boolean = true
+  mostrarMensajeNoExistenCambios:boolean = false
 
   //mensajes
   campoRequerido: string;
   fechaRequerida: string;
   horarioNoValido: string;
   soloNumeros: string;
+  mensajeNoExistenCambios="No detectamos cambios para actualizar"
 
   pacienteJson = {}
   //citaJson = {}
@@ -82,7 +87,7 @@ export class CitaEditComponent implements OnInit {
       const selectedDate = new Date(date.year, date.month - 1, date.day);
 
       // Deshabilitar los días domingos y fechas anteriores a la actual
-      return selectedDate.getDay() === 0 || selectedDate < currentDate;
+      return selectedDate.getDay() === 0 || selectedDate < new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
     };
     }
 
@@ -100,34 +105,39 @@ export class CitaEditComponent implements OnInit {
         //console.log(this.citaEditar);
 
         this.motivo = this.citaEditar.motivo
+        this.motivoObtenido = this.citaEditar.motivo
         this.nombrePaciente = this.citaEditar.nombre
         this.apellidoPaternoPaciente = this.citaEditar.apellidop
         this.apellidoMaternoPaciente = this.citaEditar.apellidom
         this.edadPaciente = this.citaEditar.edad
         this.telefonoPaciente = this.citaEditar.telefono
+        this.telefonoPacienteObtenido = this.citaEditar.telefono
         this.id_paciente=this.citaEditar.id_paciente
 
         //this.fechaModelInicio = { year: 1789, month: 7, day: 14 };
         const fechaInicioCita: NgbDateStruct = this.ngbDateParserFormatter.parse(this.citaEditar.start);
         this.fechaModelInicio = fechaInicioCita
+        this.fechaModelInicioObtenido = fechaInicioCita;
 
         console.log("Start: "+this.citaEditar.start)
         const [fecha, horaStar] = this.citaEditar.start.split(' ');
         const [hourStart, minuteStart, second] = horaStar.split(':');
         this.selectedTimeInicio = { hour: +hourStart, minute: +minuteStart };
+        this.selectedTimeInicioObtenido = { hour: +hourStart, minute: +minuteStart };
 
         if(this.citaEditar.end){
           
           const fechaFinCita: NgbDateStruct = this.ngbDateParserFormatter.parse(this.citaEditar.end);
           this.fechaModelFin = fechaFinCita
+          this.fechaModelFinObtenido = fechaFinCita;
+
           console.log("End: "+this.citaEditar.end)
           const [fechaEnd, horaEnd] = this.citaEditar.end.split(' ');
           const [hourEnd, minuteEnd, secondEnd] = horaEnd.split(':');
           this.selectedTimeFin = { hour: +hourEnd, minute: +minuteEnd };
+          this.selectedTimeFinObtenido = { hour: +hourEnd, minute: +minuteEnd };
         }
 
-        this.validaNombre()
-        this.validaApPaterno()
         this.validaMotivo()
         this.validarFechaInicio()
         this.validaHoraInicio()
@@ -136,8 +146,56 @@ export class CitaEditComponent implements OnInit {
       }, err => console.log("error: " + err));
       
     }
+  
+  // Método para actualizar la cita
+  compararDatos(): void {
+    if (this.esIgualCita()) {
+      this.mostrarMensajeNoExistenCambios = true
+    } else {
+      this.mostrarMensajeNoExistenCambios = false
+      this.updateCita();
+    }
+
+    if (this.esIgualPaciente()) {
+      this.mostrarMensajeNoExistenCambios = true
+    } else {
+      this.mostrarMensajeNoExistenCambios = false
+      this.updatePaciente()
+    }
+  }
+
+  esIgualCita(): boolean {
+    const fechaInicioIgual = this.compararFecha(this.fechaModelInicio, this.fechaModelInicioObtenido);
+    const fechaFinIgual = this.fechaModelFin && this.fechaModelFinObtenido
+      ? this.compararFecha(this.fechaModelFin, this.fechaModelFinObtenido)
+      : this.fechaModelFin === this.fechaModelFinObtenido;
+
+    const horaInicioIgual = this.compararHora(this.selectedTimeInicio, this.selectedTimeInicioObtenido);
+    const horaFinIgual = this.selectedTimeFin && this.selectedTimeFinObtenido
+      ? this.compararHora(this.selectedTimeFin, this.selectedTimeFinObtenido)
+      : this.selectedTimeFin === this.selectedTimeFinObtenido;
+
+    const motivoIgual = this.motivo === this.motivoObtenido;
+
+    return fechaInicioIgual && fechaFinIgual && horaInicioIgual && horaFinIgual && motivoIgual;
+  }
+
+  esIgualPaciente(): boolean {
+    const telefonoIgual = this.telefonoPaciente === this.telefonoPacienteObtenido;
+    return telefonoIgual;
+  }
+
+  // Función para comparar fechas
+  compararFecha(fecha1: NgbDateStruct, fecha2: NgbDateStruct): boolean {
+    return fecha1.year === fecha2.year && fecha1.month === fecha2.month && fecha1.day === fecha2.day;
+  }
+
+  // Función para comparar horas
+  compararHora(hora1: { hour: number, minute: number }, hora2: { hour: number, minute: number }): boolean {
+    return hora1.hour === hora2.hour && hora1.minute === hora2.minute;
+  }
     
-  updateCita(){
+  updatePaciente(){
     this.spinner.show();
     this.pacienteService.updatePacienteCita(
       this.id_paciente,
@@ -151,22 +209,21 @@ export class CitaEditComponent implements OnInit {
       console.log("Se actualizó la información del paciente")
       console.log(res)
 
-      setTimeout(() => {
-        Swal.fire({
-          position: 'top-end',
-          html:
-            `<h5>Paciente actualizado correctamente</h5>`+
-            `<span>${res.nombre} ${res.apellidop} ${res.apellidom}</span>`, 
-          showConfirmButton: false,
-          backdrop: false,
-          width: 400,
-          background: 'rgb(40, 167, 69, .90)',
-          color:'white',
-          timerProgressBar:true,
-          timer: 3000,
-        })
-      }, 3000);
-
+      this.router.navigate(['/calendario'])
+      Swal.fire({
+        position: 'top-end',
+        html:
+          `<h5>Paciente actualizado correctamente</h5>`+
+          `<span>${res.nombre} ${res.apellidop} ${res.apellidom}</span>`, 
+        showConfirmButton: false,
+        backdrop: false,
+        width: 400,
+        background: 'rgb(40, 167, 69, .90)',
+        color:'white',
+        timerProgressBar:true,
+        timer: 3000,
+      })
+        
     },
     err =>{
       this.spinner.hide();
@@ -181,7 +238,11 @@ export class CitaEditComponent implements OnInit {
       })
     })
 
+  }  
+
+  updateCita(){
     this.fecha_hora_inicio = this.fechaModelInicio.year+"-"+this.fechaModelInicio.month+"-"+this.fechaModelInicio.day+" "+this.selectedTimeInicio.hour+":"+this.selectedTimeInicio.minute+":00"
+    console.log("fecha_hora_inicio a guardar: " +this.fecha_hora_inicio)
 
     if(this.fechaModelFin){
       this.fecha_hora_fin = this.fechaModelFin.year+"-"+this.fechaModelFin.month+"-"+this.fechaModelFin.day+" "+this.selectedTimeFin.hour+":"+this.selectedTimeFin.minute+":"+":00"
@@ -234,7 +295,7 @@ export class CitaEditComponent implements OnInit {
           timer: 3000
         })
       })
-  }  
+  }
 
   buscarPacientes() {
     this.pacienteService.buscarPacientes(localStorage.getItem('_cli'), this.query).subscribe(
@@ -269,33 +330,7 @@ export class CitaEditComponent implements OnInit {
     this.query=nombre +" "+apellidop+" "+apellidom 
     this.id_paciente = id_seleccionado
    
-    this.validaNombre()
-    this.validaApPaterno()
     this.validaTelefono()
-  }
-
-  validaNombre(){
-    if(this.nombrePaciente==""){
-      this.mostrarMensajeNombre = true
-    }else{
-      this.mostrarMensajeNombre = false
-    }
-  }
-
-  validaApPaterno(){
-    if(this.apellidoPaternoPaciente==""){
-      this.mostrarMensajeApPaterno = true
-    }else{
-      this.mostrarMensajeApPaterno = false
-    }
-  }
-
-  validaTitulo(){
-    if(this.titulo==""){
-      this.mostrarMensajeTitulo = true
-    }else{
-      this.mostrarMensajeTitulo = false
-    }
   }
 
   validaMotivo(){
